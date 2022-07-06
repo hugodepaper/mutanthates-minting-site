107a83eaaafa249e1b87e9054ed9af3a0db819d7
hugodepayn <86520082+hugodepaper@users.noreply.github.com>
2022-07-06 14:29:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .FreeMint(mintAmount)
+      .mint(mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
